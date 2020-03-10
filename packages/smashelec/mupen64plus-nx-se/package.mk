71bf6c5ea8f433cf7e7f59d3589437396b3d0101
CEnnis91 <cennis91@gmail.com>
2020-03-10 15:45:36 +0000

    Build mupen64plus-nx-se against develop base Change platform to match changes in Makefile Add retroarch info file for core

--- packages/smashelec/mupen64plus-nx-se/package.mk
+++ packages/smashelec/mupen64plus-nx-se/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present CEnnis91 (https://github.com/cennis91)
 
 PKG_NAME="mupen64plus-nx-se"
-PKG_VERSION="f5e7017038cd8987167d69f47ac785c49b44c682"
-PKG_SHA256="7fe4c3f1466e568e362b111883eac6bcf3dc4bd60d4241e854ea65934b67f3b2"
+PKG_VERSION="e90501a315fd91e7d9eab221127901e25839440f"
+PKG_SHA256="a16924150fb4ec32ae23ed5868e113cfeef572da481185378a2181acd96b7df3"
 PKG_ARCH="arm"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/smash64-dev/mupen64plus-libretro-nx-se/"
@@ -22,11 +22,7 @@ pre_configure_target() {
 }
 
 if [ ${PROJECT} = "Amlogic-ng" ]; then
-	PKG_MAKE_OPTS_TARGET+=" platform=AMLG12 GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm"
-elif [ "${PROJECT}" = "Amlogic" ]; then
-	PKG_MAKE_OPTS_TARGET+=" platform=AMLGX GLES=1 FORCE_GLES=1 HAVE_NEON=1 WITH_DYNAREC=arm"
-elif [ "${DEVICE}" = "OdroidGoAdvance" ]; then
-	PKG_MAKE_OPTS_TARGET+=" platform=odroidgoa"
+	PKG_MAKE_OPTS_TARGET+=" platform=AMLGX"
 fi
 
 PKG_MAKE_OPTS_TARGET+=" CORE_NAME=\"mupen64plus-smash\""
@@ -34,4 +30,5 @@ PKG_MAKE_OPTS_TARGET+=" CORE_NAME=\"mupen64plus-smash\""
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
   cp mupen64plus_next_smash_libretro.so $INSTALL/usr/lib/libretro/
+  cp $PKG_DIR/config/mupen64plus_next_smash_libretro.info $INSTALL/usr/lib/libretro/
 }
